f2f71b1e2acc46723b41af8b6590e5fa202eba7f
danuja01 <it21023378@my.sliit.lk>
2023-04-26 09:47:53 +0530

    Initial Commit

---
+++ Danuja.TasksUITests/Danuja_TasksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Danuja_TasksUITestsLaunchTests.swift
+//  Danuja.TasksUITests
+//
+//  Created by danuja Jayasuriya on 4/26/23.
+//
+
+import XCTest
+
+final class Danuja_TasksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
