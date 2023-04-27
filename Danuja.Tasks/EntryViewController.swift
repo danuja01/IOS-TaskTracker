c38de1a06529e18071ddaf04293dc7408ba44aa6
danuja01 <it21023378@my.sliit.lk>
2023-04-28 00:14:04 +0530

    initial commit

---
+++ Danuja.Tasks/EntryViewController.swift
@@ -0,0 +1,56 @@
+//
+//  EntryViewController.swift
+//  Danuja.Tasks
+//
+//  Created by danuja Jayasuriya on 4/26/23.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var field: UITextField!
+    
+    
+    var update: (() -> Void)?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
+        // Do any additional setup after loading the view.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        saveTask()
+        
+        return true
+    }
+    
+    @objc func saveTask() {
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+//         NOTE: beacuse even we know it returns Int on other side, this class dosent know it so we have to guard it
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+//
+        let newCount = count + 1
+//
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        navigationController?.popViewController(animated: true)
+        
+        update?()
+    }
+    
+
+ 
+
+}
---
+++ Danuja.Tasks/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  Danuja.Tasks
+//
+//  Created by danuja Jayasuriya on 4/26/23.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
